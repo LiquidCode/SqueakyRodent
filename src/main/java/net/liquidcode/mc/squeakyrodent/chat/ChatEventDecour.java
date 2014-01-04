7772d44bd8735f8fd5de68f89d84e9a199068921
Simen Lybekk <simen@lybekk.no>
2014-01-04 21:44:58 +0100

    Portal messages, redo level messages.

--- src/main/java/net/liquidcode/mc/squeakyrodent/chat/ChatEventDecour.java
+++ src/main/java/net/liquidcode/mc/squeakyrodent/chat/ChatEventDecour.java
@@ -1,12 +1,16 @@
 package net.liquidcode.mc.squeakyrodent.chat;
 
+import org.apache.commons.lang.WordUtils;
+
 import org.bukkit.ChatColor;
+import org.bukkit.World.Environment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 
 public class ChatEventDecour implements Listener {
     @EventHandler
@@ -33,18 +37,35 @@ public class ChatEventDecour implements Listener {
         int levelDiff = event.getNewLevel() - event.getOldLevel();
         StringBuilder message = new StringBuilder();
         if (levelDiff >= 0) {
+            message.append(ChatColor.DARK_GREEN);
+            message.append(" +");
             message.append(ChatColor.GREEN);
-            message.append(" ++");
         } else {
+            message.append(ChatColor.DARK_RED);
+            message.append(" -");
             message.append(ChatColor.RED);
-            message.append(" --");
         }
         if (Math.abs(levelDiff) > 1) {
             message.append(Math.abs(levelDiff));
         }
-        message.append(ChatColor.GRAY);
+        message.append(ChatColor.DARK_GRAY);
         message.append(" ");
         message.append(event.getPlayer().getDisplayName());
+        message.append(" -> ");
+        message.append(ChatColor.GREEN);
+        message.append(event.getPlayer().getLevel());
         event.getPlayer().getServer().broadcastMessage(message.toString());
+    }
+    
+    @EventHandler
+    public void playerPortal(PlayerPortalEvent event) {
+        Environment destination = event.getTo().getWorld().getEnvironment();
+        StringBuilder msg = new StringBuilder();
+        msg.append(ChatColor.DARK_GRAY);
+        msg.append(event.getPlayer().getDisplayName());
+        msg.append(" > ");
+        msg.append(ChatColor.GRAY);
+        msg.append(WordUtils.capitalizeFully(destination.toString()));
+        event.getPlayer().getServer().broadcastMessage(msg.toString());
     }
 }
